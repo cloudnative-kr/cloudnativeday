6a4ed75760b744a3dc1fee676be490c3ca1010ec
Kevin Jin (진겸) <tost8295@gmail.com>
2019-06-13 04:12:27 +0900

    WIP: fix styling

--- openinfraday/2019/src/components/molecules/ScheduleModal/ScheduleModal.tsx
+++ openinfraday/2019/src/components/molecules/ScheduleModal/ScheduleModal.tsx
@@ -28,7 +28,7 @@ const ContentWrapper = styled.div`
   margin: auto;
   line-height: 1.3;
   letter-spacing: -0.5px;
-  padding: 50px 0 0 0;
+  padding: 50px 0 50px 0;
 
   @media ${ON_MOBILE} {
     min-width: 310px;
@@ -39,15 +39,17 @@ const CloseButton = styled(CloseO)`
   cursor: pointer;
   position: absolute;
   display: block;
-  height: 30px;
+  height: 40px;
   line-height: 20px;
   right: 0px;
   top: 0px;
 `
 
-const FirstSection = styled.div`
+const SectionRow = styled.div`
   display: flex;
   width: 100%;
+  flex-direction: column;
+  margin-bottom: 30px;
 `
 
 const Title = styled.div`
@@ -66,8 +68,8 @@ const SpeakerRow = styled.div`
 const SpeakerInfoRow = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
-  padding-bottom: 20px;
+  justify-content: center;
+  /* padding-bottom: 20px; */
 `
 
 const SpeakerName = styled.div`
@@ -106,8 +108,6 @@ const SpeakerDescription = styled.div`
   width: 100%;
   font-size: 15px;
   margin-top: 10px;
-  border-top: 4px solid white;
-  padding-top: 10px;
   letter-spacing: 0.2px;
 
   @media ${ON_MOBILE} {
@@ -132,6 +132,25 @@ const Track = styled.div`
   }
 `
 
+const TrackDescription = styled.div`
+  width: 100%;
+  font-size: 15px;
+  margin-top: 10px;
+  border-top: 4px solid white;
+  padding-top: 10px;
+  letter-spacing: 0.2px;
+
+  @media ${ON_MOBILE} {
+    font-size: 13px;
+  }
+`
+
+const SpeakerBox = styled(Box)`
+  border-left: 2px solid white;
+  padding-left: 10px;
+  padding-right: 30px;
+`
+
 interface ScheduleModalProps {
   close: () => void
 }
@@ -139,17 +158,29 @@ interface ScheduleModalProps {
 const ScheduleModal: FC<ScheduleModalProps> = ({ close }) => (
   <>
     <ModalWrapper>
+      <CloseButton onClick={close} />
       <ContentWrapper>
         {/* <CloseButton /> */}
         <Flex flexWrap="wrap">
-          <Box width={[1, 1, 1, 50 / 100]}>
+          <SectionRow>
             <Title>Opening & OpenInfra Days Korea 2018 프로그램 소개</Title>
             <TrackInfoRow>
               <Track>4:00 pm</Track>
               <Track>Track 1</Track>
             </TrackInfoRow>
-          </Box>
-          <Box width={[1, 1, 1, 50 / 100]}>
+            <TrackDescription>
+              고가용성, 저비용, 유연하게 확장 가능한 인프라 그리고 빠른 서비스
+              딜리버리를 가능하게하기 위한 인프라의 중요성은 갈수록 증가 하고
+              있습니다. IaaS, PaaS, CaaS, SDS 영역에서 SUSE에서 서포트하는
+              프로젝트인 OpenStack, Cloud Foundry, Kubernetes, Ceph 등의
+              오픈소스 프로젝트를 활용한 SUSE의 인프라 구축/운영 전략, 사용하는
+              기술스택, 사례 그리고 HPE와의 파트너쉽을 소개 합니다.
+            </TrackDescription>
+          </SectionRow>
+          <SectionRow>
+            <Title>Speaker</Title>
+          </SectionRow>
+          <SpeakerBox width={[1, 1, 1 / 2]}>
             <SpeakerRow>
               <SpeakerImage src="speakers/cho.jpg" />
               <SpeakerInfoRow>
@@ -160,14 +191,12 @@ const ScheduleModal: FC<ScheduleModalProps> = ({ close }) => (
               </SpeakerInfoRow>
             </SpeakerRow>
             <SpeakerDescription>
-              고가용성, 저비용, 유연하게 확장 가능한 인프라 그리고 빠른 서비스
-              딜리버리를 가능하게하기 위한 인프라의 중요성은 갈수록 증가 하고
-              있습니다. IaaS, PaaS, CaaS, SDS 영역에서 SUSE에서 서포트하는
-              프로젝트인 OpenStack, Cloud Foundry, Kubernetes, Ceph 등의
-              오픈소스 프로젝트를 활용한 SUSE의 인프라 구축/운영 전략, 사용하는
-              기술스택, 사례 그리고 HPE와의 파트너쉽을 소개 합니다.
+              2007년부터 개발, 보안, 시스템 엔지니어링 등의 분야에서 경험을 쌓아
+              왔으며, 2015년도에 SUSE Korea에 입사하여 Technical Specialist로서
+              High Available하고 Scalable한 인프라를 소개 및 구축 하는 일을
+              담당하고 있습니다.
             </SpeakerDescription>
-          </Box>
+          </SpeakerBox>
         </Flex>
       </ContentWrapper>
     </ModalWrapper>
