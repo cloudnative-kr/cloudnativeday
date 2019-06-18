381c01970da45e896b3deea187ca468c04595a9b
GodofKim <kjh.original@gmail.com>
2019-06-19 03:50:39 +0900

    Add temporary alert to ticket-buying button

--- openinfraday/2019/src/App.tsx
+++ openinfraday/2019/src/App.tsx
@@ -33,7 +33,8 @@ class App extends Component {
   }
 
   handleTicketBuy = () => {
-    window.open('https://festa.io/events/41', '_blank')
+    // window.open('https://festa.io/events/41', '_blank')
+    alert('6월 21일 오픈 예정')
   }
 
   scrollTo = (element: any) => {
