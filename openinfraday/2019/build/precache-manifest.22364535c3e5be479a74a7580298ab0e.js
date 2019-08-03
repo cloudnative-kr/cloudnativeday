00d5ec6703cb4483a6a154da112798028a60aca8
Seungkyu Ahn <seungkyua@gmail.com>
2019-08-03 19:00:28 +0900

    yarn build

---
+++ openinfraday/2019/build/precache-manifest.22364535c3e5be479a74a7580298ab0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807c4aa0cf8afa7f7c9942759d9b9156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02c38af4e88196f0cd17",
+    "url": "/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "49beca2e2a6cd44af2a2",
+    "url": "/static/js/2.03d6df0a.chunk.js"
+  },
+  {
+    "revision": "02c38af4e88196f0cd17",
+    "url": "/static/js/main.8edf85bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
